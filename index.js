2b9d60279a9eb8386a0dcfe2f71b9f72523b9d13
Firstboy <zeems1234@gmail.com>
2020-10-09 14:50:00 +0500

    add welcome header and clear console before running script

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
+const welcome = require('cli-welcome');
+const pkgJSON = require('./package.json');
+
+welcome({
+  title: pkgJSON.name,
+  tagLine: 'Howdy, nice to meet ya!',
+  description: pkgJSON.description,
+  version: pkgJSON.version,
+  clear: true,
+});
+
 console.log('Zeehan Ahmad - Software Engineer');
---
+++ readne.md
@@ -0,0 +1,11 @@
+# `npx zeeshan`
+
+> Get to know Zeeshan Ahmad using your command line with node.js
+
+## Usage
+
+to use this CLI run the following command:
+
+```sh
+npx zeeshan
+```
